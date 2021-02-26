b4f57be637855b327f863d3dc651274214697107
unknown <saifalam763155@gmail.com>
2021-02-26 22:33:11 +0530

    ch

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,7 @@ const find = document.getElementById('find');
 const matchList = document.getElementById('match-list');
 //search states.json and filter it
 const searchStates = async searchText =>{
-    const res = await fetch(`../data/states.json`);
+    const res = await fetch(`/data/states.json`);
     const states = await res.json();
     //Get matches to current text input
     let matches = states.filter(state =>{
