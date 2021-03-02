2378213eb8599b2557f015cc4b7850321102e69d
mrsaif7631 <68838304+mrsaif7631@users.noreply.github.com>
2021-03-02 17:02:46 +0530

    Update main.js
    hi

--- JS/main.js
+++ JS/main.js
@@ -3,7 +3,7 @@ const find = document.getElementById('find');
 const matchList = document.getElementById('match-list');
 //search states.json and filter it
 const searchStates = async searchText =>{
-    const res = await fetch(`state-capital-app/data/states.json`);
+    const res = await fetch(`/state-capital-app/data/states.json`);
     const states = await res.json();
     //Get matches to current text input
     let matches = states.filter(state =>{
