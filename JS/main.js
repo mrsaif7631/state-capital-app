32b07f060f1b467d3d3e98bcc3932832ec839a20
unknown <saifalam763155@gmail.com>
2021-03-02 12:47:12 +0530

    ch

--- JS/main.js
+++ JS/main.js
@@ -3,7 +3,7 @@ const find = document.getElementById('find');
 const matchList = document.getElementById('match-list');
 //search states.json and filter it
 const searchStates = async searchText =>{
-    const res = await fetch(`state-capital-app/data/states.json`);
+    const res = await fetch(`/data/states.json`);
     const states = await res.json();
     //Get matches to current text input
     let matches = states.filter(state =>{
@@ -21,7 +21,7 @@ matchList.innerHTML='';
 const outputHtml = matches => {
     if(matches.length >0){
         const html=matches.map(match => `
-        <div class="output">${match.name}(${match.abbr}) ${match.capital}</div>`).join('');
+        <div class="output">${match.name}(${match.abbr}) <span class="ch-color">${match.capital}</span></div>`).join('');
         matchList.innerHTML=html;
     }
 };
